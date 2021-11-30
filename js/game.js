eee5397eed16e45018816d6e344b965ff9dc4d03
Andrew Glanz <glanzad@mail.uc.edu>
2021-11-30 14:38:23 -0500

    testing sprites

--- js/game.js
+++ js/game.js
@@ -1,1 +1,21 @@
 let gameScene = new Phaser.Scene('Game');
+
+gameScene.preload = function () {
+    // preloading our sprites
+    this.load.image('background', 'assets/background.png');
+
+    this.load.image('player', 'assets/player.png');
+    
+}
+
+gameScene.create = function() {
+    // adding background to page
+    let background = this.add.sprite(0,0, 'background');
+
+    //setting origin to top left
+    background.setOrigin(0,0);
+
+    // adding player
+    this.player = this.add.sprite(180, 10, 'player');
+
+}

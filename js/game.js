8ca89119d4fe97c8c6d6332a1d9f1a3803c52a4a
najohnson11936 <najohnson11936@outlook.com>
2021-12-10 21:42:38 -0500

    in progress enemy movement

--- js/game.js
+++ js/game.js
@@ -43,18 +43,6 @@ gameScene.create = function() {
     //these groups are for movement since different enemies should have different speeds
 
     // adding enemies for scaling purposes, location is obviously not finalized
-    this.rocket = this.add.sprite(50, 20, 'rocket');
-    this.asteroid1 = this.add.sprite(150, 20, 'asteroid1');
-    this.asteroid2 = this.add.sprite(225, 20, 'asteroid2');
-
-    // scale up enemy sprites
-    this.rocket.setScale(1.5);
-    this.asteroid1.setScale(1.5);
-    this.asteroid2.setScale(1.5);
-
-    this.enemies.add(this.rocket);
-    this.enemies.add(this.asteroid1);
-    this.enemies.add(this.asteroid2);
 
     // assign keys
     this.keys = this.input.keyboard.addKeys({
@@ -79,45 +67,59 @@ gameScene.update = function() {
     } else if (this.keys.s.isDown) {
 
     }
+
+    this.addEnemy();
+    this.moveEnemies();
 }
 
 // add enemies
 gameScene.addEnemy = function(){
     if (enemiesOnScreen < 5){
-        for (let i = enemiesOnScreen; i < 5; i++){
             this.RandomEnemy();
-        }
     }
 }
 
 //random enemy generation
 gameScene.RandomEnemy = function(){
-    var xcoord;
+    var xcoord = Math.floor(Math.random() * (350 - 1 + 1) + 1);
     var ycoord = 20;
-    var rand = Math.floor(Math.random() * 2);
+    var rand = Math.floor(Math.random() * 3);
     var enemy;
+
     switch (rand){
          case 0:
-             enemy = game.add.sprite(xcoord, ycoord, 'asteroid1');
+             enemy = this.physics.add.sprite(xcoord, ycoord, 'asteroid1');
              break;
          case 1:
-             enemy = game.add.sprite(xcoord, ycoord, 'asteroid2');
+             enemy = this.physics.add.sprite(xcoord, ycoord, 'asteroid2');
              break;
          case 2:
-             enemy = game.add.sprite(xcoord, ycoord, 'rocket');
+             enemy = this.physics.add.sprite(xcoord, ycoord, 'rocket');
              break;
-     }
-    enemiesOnScreen++;
-    this.enemies.add(enemy);
+    }
 
-    enemy.checkWorldBounds = true;
-    enemy.outOfBoundsKill = true;
+    enemiesOnScreen++;
+    enemy.setVelocity(1);
+    enemy.setCollideWorldBounds(true);
+    this.enemies.add(enemy);
 }
 
 //Enemy movement, designed to be called by the update function
-gameScene.moveEnemies = function(){
+gameScene.moveEnemies = function() {
     //Enemy movement goes here
+    let enemies = this.enemies.getChildren();
+    let numEnemies = enemies.length;
 
+    for (let i = 0; i < numEnemies; i++) {
+        enemies[i].x += 1;
+        enemies[i].y += 1;
+
+        if (enemies[i].x >= this.enemyMaxX && enemies[i].speed > 0) {
+            enemies[i].speed *= -1;
+        } else if (enemies[i].y <= this.enemyMinX && enemies[i].speed < 0) {
+            enemies[i].speed *= 1;
+        }
+    }
 }
 
 // game config
